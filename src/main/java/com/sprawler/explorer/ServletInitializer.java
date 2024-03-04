48e24a05684020cb122c7bee57522f612a52d1f7
theopin <theopinto98@gmail.com>
2024-03-04 12:37:10 +0800

    Add base content

---
+++ src/main/java/com/sprawler/explorer/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.sprawler.explorer;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ExplorerApplication.class);
+	}
+
+}
