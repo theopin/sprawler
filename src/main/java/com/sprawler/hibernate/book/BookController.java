00b56c0b543b0575c9f003f454ca949e3613129f
theopin <theopinto98@gmail.com>
2024-03-05 20:10:48 +0800

    Clean up

--- src/main/java/com/sprawler/hibernate/book/BookController.java
+++ src/main/java/com/sprawler/hibernate/book/BookController.java
@@ -1,22 +1,23 @@
-package com.sprawler.hibernate;
+package com.sprawler.hibernate.book;
 
 import com.sprawler.hibernate.book.Book;
 import com.sprawler.hibernate.book.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/hibernate")
-public class HibernateController {
-
+@RestController("bookController")
+@RequestMapping("/books")
+public class BookController {
     @Autowired
     @Qualifier("bookService")
     private BookService bookService;
 
-    @GetMapping("/books")
+    @GetMapping
     public List<Book> getAllBookList() {
         return bookService.list();
     }
