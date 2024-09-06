a4e85adcca03e9b8030597b3327e7315a1acfb3f
theopin <theopinto_tg104@hotmail.com>
2024-09-06 23:06:40 +0800

    Add health check

--- backend/src/main/java/com/sprawler/health/HealthController.java
+++ backend/src/main/java/com/sprawler/health/HealthController.java
@@ -1,7 +1,6 @@
 package com.sprawler.rest;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
