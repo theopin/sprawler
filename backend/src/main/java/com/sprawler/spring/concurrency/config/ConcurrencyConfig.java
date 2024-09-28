9df0623396fc75b8d20f4426a1af45ac01488241
theopin <theopinto_tg104@hotmail.com>
2024-09-28 08:14:44 +0800

    Update checkstyle

--- backend/src/main/java/com/sprawler/spring/concurrency/config/ConcurrencyConfig.java
+++ backend/src/main/java/com/sprawler/spring/concurrency/config/ConcurrencyConfig.java
@@ -1,7 +1,6 @@
 package com.sprawler.spring.concurrency.config;
 
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
