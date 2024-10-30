6e0806a223d7bc26bbf120e57b8315d34ffe1e92
theopin <theopinto_tg104@hotmail.com>
2024-10-30 18:14:45 +0800

    Add impl to for forming auth api call

--- backend/src/main/java/com/sprawler/external/myinfo/MyInfoController.java
+++ backend/src/main/java/com/sprawler/external/myinfo/MyInfoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Optional;
 
@@ -23,7 +24,7 @@ public class MyInfoController {
     @Qualifier("myInfoTemplate")
     RestTemplate myInfoTemplate;
 
-    @GetMapping("/person/sandbox/{uinfin}")
+    @GetMapping("/sandbox/person/{uinfin}")
     public DecryptedPersonInfo getPersonDataFromSandbox(@PathVariable("uinfin") String uinFin) {
         LOGGER.info("Running sandbox api to retrieve person data");
 
@@ -34,4 +35,28 @@ public class MyInfoController {
         return personData;
     }
 
+    @GetMapping("/authorize")
+    public String makeAuthoriseCall() {
+        LOGGER.info("Making api call to authorize data");
+
+        String authApiUrl = "https://test.api.myinfo.gov.sg/com/v4/authorize";
+        String clientId = "STG2-MYINFO-SELF-TEST";
+        String scope = "uinfin name sex race nationality dob email mobileno regadd housingtype hdbtype marital edulevel noa-basic ownerprivate cpfcontributions cpfbalances";
+        String purposeId = "demonstration";
+        String codeChallenge = "YMDc6r_HboHPuCed_ctZw9bOPqa_TcO6QJa3AyCknvQ";
+        String codeChallengeMethod = "S256";
+        String redirectUri = "http://localhost:3001/callback";
+        String responseType = "code";
+
+
+        return UriComponentsBuilder.fromHttpUrl(authApiUrl)
+                .queryParam("client_id", clientId)
+                .queryParam("scope", scope)
+                .queryParam("redirect_uri", redirectUri)
+                .queryParam("response_type", responseType)
+                .queryParam("code_challenge", codeChallenge)
+                .queryParam("code_challenge_method", codeChallengeMethod)
+                .queryParam("purpose_id", purposeId)
+                .toUriString();
+    }
 }
