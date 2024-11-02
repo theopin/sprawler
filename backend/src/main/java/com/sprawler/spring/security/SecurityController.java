1b6b03b59211717d8c520542920c84fc1dd4ca61
theopin <theopinto_tg104@hotmail.com>
2024-11-02 22:06:39 +0800

    Fix checkstyle issues

--- backend/src/main/java/com/sprawler/spring/security/SecurityController.java
+++ backend/src/main/java/com/sprawler/spring/security/SecurityController.java
@@ -1,13 +1,8 @@
 package com.sprawler.spring.security;
 
 
-import com.sprawler.spring.jms.JmsRequest;
-import com.sprawler.spring.jms.JmsResponse;
-import com.sprawler.spring.jms.JmsSender;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
--- backend/src/main/java/com/sprawler/spring/security/config/SecurityConfig.java
+++ backend/src/main/java/com/sprawler/spring/security/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
